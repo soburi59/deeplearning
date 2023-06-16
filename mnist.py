362e886610cd4e722cf9bb29bb895e97515c61cb
soburi59 <591959ant@gmail.com>
2023-06-16 16:56:40 +0900

    update

--- mnist.py
+++ mnist.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 09:52:16 2023
+cc
+"""
+
 import time
 import torch
 import torch.nn as nn
@@ -9,30 +15,10 @@ import os
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 
-# バッチサイズ (一度の学習におけるデータの数)
-BATCHSIZE = 1200
 # データを何回学習するか
 EPOCH = 10
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-loss_list = []
-accuracy_list = []
 epoch_list = list(np.arange(0, EPOCH, 1))
-
-# MNISTデータセットの読み込み
-train_dataset = tv.datasets.MNIST(root="./",
-                                  train=True,
-                                  transform=tv.transforms.ToTensor(),
-                                  download=True)
-
-test_dataset = tv.datasets.MNIST(root="./",
-                                 train=False,
-                                 transform=tv.transforms.ToTensor(),
-                                 download=True)
-
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                          batch_size=BATCHSIZE,
-                                          shuffle=False)
 
 
 class MNIST(nn.Module):
@@ -43,94 +29,110 @@ class MNIST(nn.Module):
         self.l3 = nn.Linear(300, 10)  # 最終層300→10
 
     def forward(self, x):
+        x = x.view(x.size(0), -1)
         h = F.relu(self.l1(x))
         h = F.relu(self.l2(h))
         y = self.l3(h)
         return y
 
 
-def train(train_loader, flag):
-    start_time = time.perf_counter()
+def train_and_test(optimizer_name, batch_size, epochs):
+    loss_list = []
+    accuracy_list = []
+
+    # DataLoaderの作成
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+    # モデルの初期化
     model = MNIST().to(DEVICE)
-    optimizer = torch.optim.Adam(model.parameters()) if flag else torch.optim.SGD(model.parameters(), lr=0.1)
-    model.train()
-    for epoch in range(EPOCH):
+    criterion = nn.CrossEntropyLoss()
+
+    # Optimizerの設定
+    if optimizer_name == 'Adam':
+        optimizer = torch.optim.Adam(model.parameters())
+    elif optimizer_name == 'SGD':
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+    else:
+        raise ValueError("Invalid optimizer specified.")
+    
+    #train
+    for epoch in range(epochs):
+        start_time = time.perf_counter()
+        model.train()
         total_loss = 0
+
         for images, labels in train_loader:
-            images = images.view(-1, 28*28).to(DEVICE)
+            images = images.view(-1, 28 * 28).to(DEVICE)
             labels = labels.to(DEVICE)
             optimizer.zero_grad()
-            y = model(images)
-            loss = F.cross_entropy(y, labels)
-            total_loss += loss.item()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-        loss_list.append(float(total_loss))
-        if epoch == 1:
-            print(f"epoch1のときのloss={total_loss}")
-    print(f"学習処理時間: {time.perf_counter() - start_time}s")
+            total_loss += loss.item()
+
+        loss_list.append(total_loss)
+        
+        #test
+        model.eval()
+        total = len(test_loader.dataset)
+        correct = 0
+
+        with torch.no_grad():
+            for images, labels in test_loader:
+                images = images.view(-1, 28 * 28).to(DEVICE)
+                labels = labels.to(DEVICE)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                correct += (predicted == labels).sum().item()
+
+        accuracy = correct / total
+        accuracy_list.append(accuracy)
+        print(f"Epoch [{epoch+1}/{epochs}], Accuracy: {accuracy:.4f}, Loss: {total_loss:.4f}, Time: {time.perf_counter() - start_time:.2f}s")
+    #トレーニングしたモデルの出力
+    torch.save(model.state_dict(), f"model/model_batch{batch_size}_{optimizer_name}.pth")
+    save_fig(batch_size, optimizer_name, loss_list, accuracy_list)
 
 
-def test(test_loader, i):
-    total = len(test_loader.dataset)
-    correct = 0
-    model = MNIST().to(DEVICE)
-    model.load_state_dict(torch.load(f"epoch_{i}.model"))
-    model.eval()
-    with torch.no_grad():
-        for images, labels in test_loader:
-            images = images.view(-1, 28*28).to(DEVICE)
-            labels = labels.to(DEVICE)
-            y = model(images)
-            pred_labels = y.max(dim=1)[1]
-            correct += (pred_labels == labels).sum()
-    accuracy_list.append(correct.item() / total)
-
-
-def save_fig(i, flag):
+def save_fig(batch_size, optimizer_name, loss_list, accuracy_list):
+    # グラフの作成
     fig, (ax1, ax2) = plt.subplots(1, 2)
-    fig.suptitle(f"BATCHSIZE={i}")
-    ax1.set_title("loss_graph")
-    ax1.set_xlabel("epoch")
-    ax1.set_ylabel("loss")
+    fig.suptitle(f"BATCHSIZE={batch_size}, Optimizer={optimizer_name}")
+
+    # Lossグラフの設定
+    ax1.set_title("Loss Graph")
+    ax1.set_xlabel("Epoch")
+    ax1.set_ylabel("Loss")
     ax1.set_ylim(0, 100)
     ax1.plot(loss_list)
     ax1.set_xticks(epoch_list)
 
-    ax2.set_title("accuracy_graph")
-    ax2.set_xlabel("epoch")
-    ax2.set_ylabel("accuracy")
+    # Accuracyグラフの設定
+    ax2.set_title("Accuracy Graph")
+    ax2.set_xlabel("Epoch")
+    ax2.set_ylabel("Accuracy")
     ax2.set_ylim(0.9, 1)
     ax2.plot(accuracy_list)
     ax2.set_xticks(epoch_list)
 
+    # グラフのレイアウト調整
     fig.tight_layout()
-    plt.savefig(f"graph_batch{i}.png")
-   
+    # グラフの保存
+    plt.savefig(f"graph/graph_batch{batch_size}_{optimizer_name}.png")
 
 
-batch_sizes = [2**n for n in range(5, 11)]
-print("---------------")
-for batch_size in batch_sizes:
-    print(f"BATCHSIZE={batch_size}")
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                               batch_size=batch_size,
-                                               shuffle=True)
-    train(train_loader, True)
-    for j in range(EPOCH):
-        test(test_loader, j)
-    save_fig(batch_size, True)
-    loss_list = []
-    accuracy_list = []
+if __name__ == '__main__':
+    # MNISTデータセットの読み込み
+    train_dataset = tv.datasets.MNIST(root="./", train=True, transform=tv.transforms.ToTensor(), download=True)
+    test_dataset = tv.datasets.MNIST(root="./", train=False, transform=tv.transforms.ToTensor(), download=True)
 
-print("---------------")
-print("Adam → SGD")
-batch_size = 256
-print(f"BATCHSIZE={batch_size}")
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=batch_size,
-                                           shuffle=True)
-train(train_loader, False)
-for j in range(EPOCH):
-    test(test_loader, j)
-save_fig(batch_size, False)
+    batch_sizes = [2 ** n for n in range(5, 11)]
+    optimizers = ['Adam', 'SGD']
+    print("---------------")
+    for batch_size in batch_sizes:
+        print(f"BATCHSIZE={batch_size}")
+        for optimizer in optimizers:
+            print(f"Optimizer: {optimizer}")
+            train_and_test(optimizer, batch_size, EPOCH)
+        print("---------------")
--- use_model.py
+++ use_model.py
@@ -1,17 +1,23 @@
 import torch
 import torch.nn.functional as F
 import torchvision.transforms as transforms
-from PIL import Image
+from mnist import MNIST
+from PIL import Image, ImageOps
 
 def load_and_predict(image_path,model_path):
     # モデルの読み込み
-    model = MyModelDefinition(args)
-    model.load_state_dict(torch.load(model_path)
+    state_dict = torch.load(model_path)
+    model = MNIST()  # モデルのインスタンスを作成する
+    model.load_state_dict(state_dict)
     model.eval()
 
     # 画像の前処理
-    image = Image.open(image_path).convert("L")  # グレースケールに変換
-    transform = transforms.ToTensor()
+    image = Image.open(image_path)
+    image = ImageOps.invert(image.convert('L')).resize((28,28))
+    transform = transforms.Compose([
+        transforms.ToTensor(),  # テンソルに変換
+        transforms.Normalize((0.0,), (1.0,))  # 正規化
+    ])
     input_image = transform(image).unsqueeze(0)
 
     # 予測の実行
@@ -23,8 +29,8 @@ def load_and_predict(image_path,model_path):
     return zip(range(10), probabilities.squeeze().tolist())
 
 if __name__ == '__main__':
-    image_path = "test_image.png"
-    model_path = "mode.pth"
+    image_path = "data_image/1.png"
+    model_path = "model_batch256_SGD.pth"
     class_probabilities = load_and_predict(image_path,model_path)
     
     # 結果の表示
